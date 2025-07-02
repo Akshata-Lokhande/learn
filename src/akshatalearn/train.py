f479fe84f7841aeb9f768eae9c81a7c58e7bd216
Akshata-Lokhande <87219643+Akshata-Lokhande@users.noreply.github.com>
2025-07-02 17:09:14 +0200

    Update train.py

--- src/akshatalearn/train.py
+++ src/akshatalearn/train.py
@@ -2,7 +2,7 @@ from akshatalearn.model import Model
 from akshatalearn.data import MyDataset
 
 def train():
-    dataset = MyDataset("corruptmnist_v1")
+    dataset = MyDataset("/Users/akshata/Documents/Githublearn/learn/corruptmnist_v1")
     model = Model()
     # add rest of your training code here
 
