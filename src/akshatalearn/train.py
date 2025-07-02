2a6c397245453ce3c0c45fef0f8c826998ce420b
Akshata Lokhande <akshatalokhande03@gmail.com>
2025-07-02 14:59:41 +0200

    First cokkiecutter

---
+++ src/akshatalearn/train.py
@@ -0,0 +1,10 @@
+from akshatalearn.model import Model
+from akshatalearn.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
